cf83b3f8053d12f3fdef089879b518adc098cdad
Charlie McClung <charlie@cmr1.com>
2019-04-30 11:44:27 -0600

    Updates for auto dev db setup

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM phusion/baseimage
 
 RUN curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | bash
 
-RUN	\
-  apt-get update \
-  &&	apt-get -y upgrade \
-  &&	apt-get update --fix-missing
+RUN set -ex && \
+      apt-get update \
+      && apt-get -y upgrade \
+      && apt-get update --fix-missing
 
-RUN \
-    apt-get install -y \
+RUN set -ex && \
+      apt-get install -y \
       php7.0 \
       php7.0-bcmath \
       php7.0-cli \
@@ -30,15 +30,22 @@ RUN curl -sS https://getcomposer.org/installer | php
 RUN mv composer.phar /usr/local/bin/composer
 RUN service php7.0-fpm start
 
-RUN \
-    apt-get install -y \
+ENV SOPS_SHA 6b1d245c59c46b0f7c1f5b9fa789e0236bdcb44b0602ca1a7cadb6d0aac64c3c
+
+ADD https://github.com/mozilla/sops/releases/download/3.0.5/sops_3.0.4_amd64.deb /tmp/sops.deb
+
+RUN set -ex && \
+      # Verify downloaded sops.deb SHA
+      echo "$SOPS_SHA /tmp/sops.deb" | sha256sum -c - && \
+      # Install sops from downloaded .deb file
+      dpkg -i /tmp/sops.deb && \
+      # Remove tmp install files
+      rm /tmp/sops.deb && \
+      apt-get install -y \
       nodejs \
       npm \
       git \
-      mysql-client
-
-RUN \
-    apt-get install -y \
+      mysql-client \
       nginx-full \
       supervisor
 
@@ -51,10 +58,15 @@ COPY build/.bashrc /root/.bashrc
 COPY build/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 COPY build/nginx.conf /etc/nginx/sites-enabled/default
 COPY build/php.ini /etc/php/7.0/fpm/php.ini
+COPY *.sh /
+COPY .env.* /
 
 ADD db /db/
 ADD app /var/www/app/
 ADD public /var/www/public/
 
 EXPOSE 80 443
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+error() {
+  red='\033[0;31m'
+  nocolor='\033[0m'
+  prefix="======>>  "
+  suffix="  <<======"
+  echo
+  echo -e "${red}${prefix}${1}${suffix}${nocolor}"
+  echo
+}
+
+log() {
+  yellow='\033[0;33m'
+  nocolor='\033[0m'
+  prefix="======>>  "
+  suffix="  <<======"
+  echo
+  echo -e "${yellow}${prefix}${1}${suffix}${nocolor}"
+  echo
+}
+
+fail() {
+  msg=${1:-Failure}
+  code=${2:-1}
+
+  error "FATAL: $msg"
+
+  exit $code
+}
+
+has_env_var() {
+  env | grep "$1" > /dev/null 2>&1
+  return $?
+}
+
+missing_env_var() {
+  if has_env_var $1; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+APP_ENV=${APP_ENV:-development}
+ENV_FILE="/.env.$APP_ENV"
+
+if [ ! -f $ENV_FILE ]; then
+  fail "Missing ENV file: $ENV_FILE"
+fi
+
+ENV_VARS=$(sops -d $ENV_FILE 2> /dev/null)
+
+if [[ "$?" != "0" ]]; then
+  ENV_VARS=$(cat $ENV_FILE)
+fi
+
+export $(echo $ENV_VARS | xargs)
+
+REQUIRED_ENV_VARS="
+DATABASE_HOST
+DATABASE_PORT
+DATABASE_USER
+DATABASE_PASS
+DATABASE_NAME
+"
+
+for v in $REQUIRED_ENV_VARS; do
+  if missing_env_var "$v"; then
+    fail "Missing required ENV VAR: '$v'"
+  fi
+done
+
+/wait-for-it.sh $DATABASE_HOST:$DATABASE_PORT -t 30
+
+if [ ! -z "$REDIS_HOST" ] && [ ! -z "$REDIS_PORT" ]; then
+  /wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 30
+fi
+
+if [[ "$APP_ENV" == "development" ]]; then
+  log "App is running in development, attempt to initialize database ..."
+
+  mysql -h ${DATABASE_HOST} -u root -p${MYSQL_ROOT_PASSWORD} -e "set @@global.sql_mode = '';"
+  (\
+    mysql -h ${DATABASE_HOST} -u root -p${MYSQL_ROOT_PASSWORD} -e "create database ${DATABASE_NAME}" \
+    && mysql -h ${DATABASE_HOST} -u root -p${MYSQL_ROOT_PASSWORD} ${DATABASE_NAME} < /db/${APP_ENV}.seed.sql\
+  ) || log "Using existing DB: ${DATABASE_NAME} | Reset this by running 'docker volume rm phalcontutorial_db'"
+fi
+
+exec "$@"
